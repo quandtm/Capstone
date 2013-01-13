c71a1517ee9a7b6b837774f2c2f1f6e762edd50c
Michael Quandt <michael@mquandt.com>
2013-01-14 02:40:03 +1100

    Further work on the editor UI

--- Capstone.Editor/Views/EditorPage.xaml.cs
+++ Capstone.Editor/Views/EditorPage.xaml.cs
@@ -14,5 +14,25 @@ namespace Capstone.Editor.Views
             var proxy = App.CurrentApp.Direct3D;
             proxy.SetPanel(swapPanel);
         }
+
+        private void OpenBuildMode(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenLandscapeMode(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeToolSelect(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeToolPan(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
