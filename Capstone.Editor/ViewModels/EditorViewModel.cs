c134efeef66ac04444051769c14b49e3a0b83f4c
Michael Quandt <michael@mquandt.com>
2013-01-18 14:20:47 +1100

    Cleaned out some old object stuff.

--- Capstone.Editor/ViewModels/EditorViewModel.cs
+++ Capstone.Editor/ViewModels/EditorViewModel.cs
@@ -1,12 +1,10 @@
 ﻿using Capstone.Core;
 using Capstone.Editor.Common;
 using Capstone.Editor.Data;
-using Capstone.Editor.Data.ObjectTemplates;
 using Capstone.Editor.Scripts;
 using Capstone.Engine.Graphics;
 using Capstone.Engine.Scripting;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Windows.Foundation;
 
 namespace Capstone.Editor.ViewModels
@@ -23,20 +21,6 @@ namespace Capstone.Editor.ViewModels
         private Point _prevPoint;
         private Entity _cam;
 
-        public ObservableCollection<BaseObjectTemplate> Objects { get; private set; }
-        private BaseObjectTemplate _selectedObj;
-        public BaseObjectTemplate SelectedObject
-        {
-            get { return _selectedObj; }
-            set
-            {
-                if (_selectedObj == value) return;
-                _selectedObj = value;
-                if (_selectedObj != null)
-                    Tool = EditorTool.Build;
-            }
-        }
-
         public int Money { get; private set; }
 
         private readonly List<Entity> _entities;
@@ -52,8 +36,6 @@ namespace Capstone.Editor.ViewModels
             {
                 if (_tool == value) return;
                 _tool = value;
-                if (_tool != EditorTool.Build)
-                    SelectedObject = null;
             }
         }
         public bool IsPanTool
@@ -70,7 +52,6 @@ namespace Capstone.Editor.ViewModels
         {
             ObjectiveManager = new ObjectiveManager();
             _entities = new List<Entity>();
-            Objects = new ObservableCollection<BaseObjectTemplate>();
 
             Money = 500;
 
@@ -100,7 +81,6 @@ namespace Capstone.Editor.ViewModels
 
         public void PopulateObjectList()
         {
-            Objects.Add(new PlayerObject());
         }
 
         internal void HandleClick(Windows.Foundation.Point point)
@@ -108,25 +88,9 @@ namespace Capstone.Editor.ViewModels
             switch (Tool)
             {
                 case EditorTool.Build:
-                    BuildSprite(point);
+                    //BuildSprite(point);
                     break;
             }
-        }
-
-        private void BuildSprite(Point point)
-        {
-            if (Money <= 0) return;
-            if (SelectedObject == null) return;
-
-            var e = SelectedObject.CreateEntityInstance();
-            if (e == null) return;
-            var screenPt = new Vector2((float)point.X, (float)point.Y);
-            CameraManager.Instance.ActiveCamera.ScreenToWorld(screenPt, e.Translation);
-            _entities.Add(e);
-            Money = Money - SelectedObject.Cost;
-
-            if (SelectedObject is PlayerObject)
-                ObjectiveManager.CompleteObjective("AddPlayer");
         }
     }
 }
