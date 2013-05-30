e97abf696019550e8bb51c0871cb573043114bfc
Michael Quandt <michael@mquandt.com>
2013-05-31 03:37:09 +1000

    Cleared out UI for revamp.

--- Capstone.Editor/Views/MainPage.xaml.cs
+++ Capstone.Editor/Views/MainPage.xaml.cs
@@ -1,66 +1,6 @@
-﻿using System;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
-
-namespace Capstone.Editor.Views
+﻿namespace Capstone.Editor.Views
 {
-    public sealed partial class MainPage : IView, INotifyPropertyChanged
+    public sealed partial class MainPage
     {
-        public bool WasInEditor { get; private set; }
-        public bool WasInGame { get; private set; }
-
-        public MainPage()
-        {
-            WasInEditor = true;
-            WasInGame = true;
-            DataContext = this;
-            InitializeComponent();
-        }
-
-        private async void OpenGame(object sender, RoutedEventArgs e)
-        {
-            var ofd = new FileOpenPicker();
-            ofd.FileTypeFilter.Add(".level");
-            ofd.CommitButtonText = "Load";
-            ofd.SuggestedStartLocation = PickerLocationId.Desktop;
-            var file = await ofd.PickSingleFileAsync();
-            if (file != null)
-            {
-                (await App.CurrentApp.Navigate<GamePage>()).Load(file);
-                WasInEditor = false;
-            }
-        }
-
-        private async void NewMap(object sender, RoutedEventArgs e)
-        {
-            (await App.CurrentApp.Navigate<EditorPage>()).NewLevel();
-            WasInGame = false;
-        }
-
-        private async void LoadMap(object sender, RoutedEventArgs e)
-        {
-            var ofd = new FileOpenPicker();
-            ofd.FileTypeFilter.Add(".level");
-            ofd.CommitButtonText = "Load";
-            ofd.SuggestedStartLocation = PickerLocationId.Desktop;
-            var file = await ofd.PickSingleFileAsync();
-            if (file != null)
-            {
-                (await App.CurrentApp.Navigate<EditorPage>()).LoadLevel(file);
-                WasInGame = false;
-            }
-        }
-
-        public async Task HandleNavigationTo(object parameter)
-        {
-        }
-
-        public async Task HandleNavigationFrom()
-        {
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
