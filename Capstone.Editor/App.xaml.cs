7413436a58dfad1c535ac85f1288eef454cc1647
Michael Quandt <michael@mquandt.com>
2013-05-31 03:54:08 +1000

    Finished cleaning up UI.

--- Capstone.Editor/App.xaml.cs
+++ Capstone.Editor/App.xaml.cs
@@ -1,29 +1,11 @@
-﻿using System.Threading.Tasks;
-using Capstone.Editor.Views;
-using Capstone.Engine.Windows;
-using System;
-using System.Collections.Generic;
+﻿using Capstone.Editor.Views;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 
 namespace Capstone.Editor
 {
     sealed partial class App
     {
-        private readonly Dictionary<Type, Page> _pages;
-        private readonly Stack<Page> _backStack;
-
-        public Direct3DPanelProxy _d3d;
-        public Direct3DPanelProxy Direct3D
-        {
-            get
-            {
-                if (_d3d == null) _d3d = new Direct3DPanelProxy();
-                return _d3d;
-            }
-        }
-
         public static App CurrentApp
         {
             get { return (App)Current; }
@@ -32,55 +14,12 @@ namespace Capstone.Editor
         public App()
         {
             InitializeComponent();
-            _pages = new Dictionary<Type, Page>();
-            _backStack = new Stack<Page>();
-
-            this.Suspending += App_Suspending;
-        }
-
-        private void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            if (Window.Current.Content != null && Window.Current.Content is IView)
-                ((IView)Window.Current.Content).HandleNavigationFrom().Wait();
-            deferral.Complete();
         }
 
         protected override async void OnLaunched(LaunchActivatedEventArgs args)
         {
-            await Navigate<MainPage>();
+            Window.Current.Content = new MainPage();
             Window.Current.Activate();
-        }
-
-        public async Task<T> Navigate<T>(object parameter = null) where T : Page, new()
-        {
-            Page p = null;
-            if (!_pages.TryGetValue(typeof(T), out p))
-            {
-                p = new T();
-                _pages.Add(typeof(T), p);
-            }
-            _backStack.Push((Page)Window.Current.Content);
-
-            if (Window.Current.Content != null && Window.Current.Content is IView)
-                await ((IView)Window.Current.Content).HandleNavigationFrom();
-            Window.Current.Content = p;
-            if (p is IView)
-                await ((IView)p).HandleNavigationTo(parameter);
-            return (T)p;
-        }
-
-        public void GoBack()
-        {
-            var back = _backStack.Pop();
-            if (back != null)
-            {
-                if (Window.Current.Content != null && Window.Current.Content is IView)
-                    ((IView)Window.Current.Content).HandleNavigationFrom();
-                Window.Current.Content = back;
-                if (back is IView)
-                    ((IView)back).HandleNavigationTo(null);
-            }
         }
     }
 }
--- Capstone.Editor/Views/MainPage.xaml.cs
+++ Capstone.Editor/Views/MainPage.xaml.cs
@@ -1,6 +1,25 @@
-﻿namespace Capstone.Editor.Views
+﻿using Capstone.Engine.Windows;
+using Windows.UI.Xaml;
+
+namespace Capstone.Editor.Views
 {
     public sealed partial class MainPage
     {
+        private Direct3DPanelProxy _proxy;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            bgPanel.Loaded += InitBGPanel;
+        }
+
+        private void InitBGPanel(object sender, RoutedEventArgs e)
+        {
+            if (_proxy == null)
+            {
+                _proxy = new Direct3DPanelProxy();
+                _proxy.SetPanel(bgPanel, false);
+            }
+        }
     }
 }
