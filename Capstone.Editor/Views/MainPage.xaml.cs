81b33e2b41a07889052a176fe4c4c77a30b17a0f
Michael Quandt <michael@mquandt.com>
2013-05-31 04:42:01 +1000

    More UI work.

--- Capstone.Editor/Views/MainPage.xaml.cs
+++ Capstone.Editor/Views/MainPage.xaml.cs
@@ -10,16 +10,46 @@ namespace Capstone.Editor.Views
         public MainPage()
         {
             InitializeComponent();
-            bgPanel.Loaded += InitBGPanel;
+            OpenMainMenu();
         }
 
-        private void InitBGPanel(object sender, RoutedEventArgs e)
+        private void InitBGPanel()
         {
             if (_proxy == null)
             {
                 _proxy = new Direct3DPanelProxy();
                 _proxy.SetPanel(bgPanel, false);
             }
+        }
+
+        private void OpenMainMenu()
+        {
+            mainMenuPanel.Visibility = Visibility.Visible;
+            editorPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenEditor()
+        {
+            InitBGPanel();
+            editorPanel.Visibility = Visibility.Visible;
+            mainMenuPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenEditorLoad(object sender, RoutedEventArgs e)
+        {
+            OpenEditor();
+        }
+
+        private void OpenEditorNew(object sender, RoutedEventArgs e)
+        {
+            OpenEditor();
+        }
+
+        private void ToggleEditorObjectives(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            EdObjectivePanel.Visibility = EdObjectivePanel.Visibility == Visibility.Visible
+                                              ? Visibility.Collapsed
+                                              : Visibility.Visible;
         }
     }
 }
