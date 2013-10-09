645bc87dd2ffaa03e2278d8bab3fbea561b0ffe1
Michael Quandt <michael@mquandt.com>
2013-10-10 04:19:30 +1100

    Added empty TileSprite class with implementation notes.

---
+++ Capstone/Capstone.Graphics/Sprites/Tilesprite.cs
@@ -0,0 +1,15 @@
+﻿namespace Capstone.Graphics.Sprites
+{
+    public class TileSprite
+    {
+        /*
+         * A sprite that is rendered out from a tilemap at runtime.
+         * The sprite itself is defined by an array of tiles
+         * and then uses a tilemap texture to render out the tiles.
+         * The tilesprite will have its own grid, but won't snap to an external grid.
+         * This allows for complex "sprites" to be built up from a tilemap and
+         * rotated or positioned as needed within the world.
+         * Ideally there will be a small form of culling to help reduce overdraw.
+         * */
+    }
+}
