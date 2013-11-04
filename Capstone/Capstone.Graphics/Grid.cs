761a65653acd44fa0b2375cdead2e38f541aeb89
Michael Quandt <michael@mquandt.com>
2013-11-05 03:31:11 +1100

    Added snapping code

---
+++ Capstone/Capstone.Graphics/Grid.cs
@@ -0,0 +1,12 @@
+﻿using SharpDX;
+
+namespace Capstone.Graphics
+{
+    public static class Grid
+    {
+        public static Vector3 Snap(Vector3 v)
+        {
+            return new Vector3((int)v.X, (int)v.Y, (int)v.Z);
+        }
+    }
+}
