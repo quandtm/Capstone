8eed8947d929179335a3c66acc6961d0710adc0a
Michael Quandt <michael@mquandt.com>
2013-11-05 03:45:14 +1100

    Further grid implementation.

--- Capstone/Capstone.Graphics/Grid.cs
+++ Capstone/Capstone.Graphics/Grid.cs
@@ -2,11 +2,55 @@
 
 namespace Capstone.Graphics
 {
-    public static class Grid
+    public class Grid
     {
-        public static Vector3 Snap(Vector3 v)
+        private Vector3 _topLeftCenter;
+        private Vector3 _topLeft;
+        private float _cellSize;
+        private int _width;
+        private int _height;
+
+        public Grid(Vector3 center, float cellSize, int width, int height)
         {
-            return new Vector3((int)v.X, (int)v.Y, (int)v.Z);
+            _cellSize = cellSize;
+            _width = width;
+            _height = height;
+            _topLeft = center - (new Vector3(width / 2, height / 2, 0) * cellSize);
+            _topLeftCenter = _topLeft + new Vector3(cellSize / 2, cellSize / 2, 0);
+        }
+
+        public bool ContainsPoint(Vector3 v)
+        {
+            int x, y;
+            ToCellCoords(v, out x, out y);
+            return x >= 0 && x < _width && y >= 0 && y < _height;
+        }
+
+        public Vector3 ToCellCenter(int x, int y)
+        {
+            return _topLeftCenter + (new Vector3(x, y, 0) * _cellSize);
+        }
+
+        public void ToCellCoords(Vector3 v, out int x, out int y)
+        {
+            x = y = 0;
+            var dir = v - _topLeft;
+            x = (int)(dir.X / _cellSize);
+            y = (int)(dir.Y / _cellSize);
+        }
+
+        public Vector2 ToCellCoords(Vector3 v)
+        {
+            int x, y;
+            ToCellCoords(v, out x, out y);
+            return new Vector2(x, y);
+        }
+
+        public Vector3 Snap(Vector3 v)
+        {
+            int x, y;
+            ToCellCoords(v, out x, out y);
+            return ToCellCenter(x, y);
         }
     }
 }
